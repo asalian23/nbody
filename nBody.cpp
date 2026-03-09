a158073b03d479bf4228c9f440d8885479cf9edf
asalian23 <aryansalian21@gmail.com>
2026-03-08 21:30:22 -0400

    Changed calcAcc to calcPairAcc to relfect what it actually does. Implemented new calcAcc function, taking in the vector of bodies and calculating the acceleration of each one. Created the leapfrog function, using leapfrog integration with Kick-Drift-Kick to show position displacement. Simulated a 3 body system including the sun, earth, and jupiter over 1 earth year with dt being 1 hour. Results were as expected, earth had the same distance from the sun before and after and jupiter slightly differed as it only went through 1/12 of its orbit. Next steps are to work on making this visual

--- nBody.cpp
+++ nBody.cpp
@@ -24,38 +24,75 @@ struct Vec3 {
 
 };
 
-struct Body {
+struct body {
     Vec3 pos;
     Vec3 vel;
     Vec3 acc;
     double mass;
 };
 
-void calcAcc(Body& b1, Body& b2){
+void calcPairAcc(body& b1, body& b2) {
     double r = (b1.pos - b2.pos).magnitude(); //distance between the two bodies
     double m1 = b1.mass;
     double m2 = b2.mass;
     double forceMag = (G * m1 * m2)/(r*r); //gravitational force magnitude between bodies, using (Gm1m2/(r^2))
-    Vec3 dir = (b1.pos - b2.pos).uVec(); //vector pointing from body 1 to body 2
+    Vec3 dir = (b2.pos - b1.pos).uVec(); //vector pointing from body 1 to body 2
     Vec3 forceVec = dir * forceMag; //multiplying direction vector from one planet to the other by the magnitude of the force to make it a vector
     b1.acc = b1.acc + forceVec * (1.0/b1.mass); //Acceleration is F/m as F=ma. Also we multiply by 1/mass because I didn't write a / operator.
     b2.acc = b2.acc - forceVec * (1.0/b2.mass); //The force vector for b2 is flipped due to Newton's third law
 }
 
-int main() {
-    Body earth;
-    earth.mass = 5.972e24;
-    earth.pos  = Vec3(1.496e11, 0, 0); // 1 AU from origin
-    earth.vel  = Vec3(0, 29783, 0);    // orbital velocity m/s
+void calcAcc(std::vector<body>& bodies) {
+    for (auto& b : bodies) { b.acc = Vec3(0, 0, 0); } //This goes through all our bodies and set the accelerations back to 0 as we need to recalculate them.
+    for (int i=0; i<bodies.size(); i++) {
+        for (int j=i+1; j<bodies.size(); j++) {
+            calcPairAcc(bodies[i], bodies[j]); //Adjusting the acceleration of each body using the pair acceleration function on each unique pair.
+        }
+    }
+}
 
-    Body sun;
+void leapfrog(std::vector<body>& bodies) {
+    for (auto& b : bodies) {
+        b.vel = b.vel + b.acc*0.5*dt; //Kick 1, velocity updated to v_(i+1/2)
+        b.pos = b.pos + b.vel*dt; //Drift, new position of bodies
+    }
+    calcAcc(bodies); //Updating acceleration of bodies to a_(i+1), needs to be outside loop
+    for (auto& b : bodies) {
+        b.vel = b.vel + b.acc*0.5*dt; //Kick 2, velocity updated to v_(i+1), check README to understand terminology used here
+    }
+}
+
+int main() {
+    std::vector<body> bodies;
+
+    body earth;
+    earth.mass = 5.972e24;
+    earth.pos  = Vec3(1.496e11, 0, 0); // 1 AU (the average distance of the earth from the sun) from origin
+    earth.vel  = Vec3(0, 29783, 0);    // orbital velocity in m/s
+
+    body sun;
     sun.mass = 1.989e30;
     sun.pos  = Vec3(0, 0, 0); 
     sun.vel  = Vec3(0, 0, 0);
 
-    std::cout << "Earth pos: " << earth.pos.x << ", " << earth.pos.y << "\n";
-    std::cout << "Sun mass:  " << sun.mass << "\n";
-    std::cout << "Everything compiled, how suprising.\n";
+    body jupiter;
+    jupiter.mass = 1.898e27;
+    jupiter.pos = Vec3(7.785e11, 0, 0);
+    jupiter.vel = Vec3(0, 13070, 0);
+
+    bodies.push_back(sun);
+    bodies.push_back(earth);
+    bodies.push_back(jupiter);
+
+    calcAcc(bodies);
+
+    for (int i=0; i<8760; i++) {
+        leapfrog(bodies);
+        double distE = (bodies[1].pos - bodies[0].pos).magnitude();
+        double distJ = (bodies[2].pos - bodies[0].pos).magnitude();
+        std::cout << "Earth is" << distE/1000.0 << "KMs away from the sun!" << std::endl;
+        std::cout << "Jupiter is" << distJ/1000.0 << "KMs away from the sun!" << std::endl;
+    }
 
     return 0;
 }
